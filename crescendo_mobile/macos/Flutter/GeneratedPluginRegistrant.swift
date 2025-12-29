6dd3cdca52ddc3d842dbab5ef8e8aeb49dbd2d17
Adrianna Wenz <adriannawenz@gmail.com>
2025-12-28 19:13:24 -0500

    Builds for Android, but not release yet

--- crescendo_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ crescendo_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import audio_session
 import audioplayers_darwin
-import mic_stream
 import package_info_plus
 import path_provider_foundation
 import record_darwin
@@ -18,7 +17,6 @@ import wakelock_plus
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  MicStreamPlugin.register(with: registry.registrar(forPlugin: "MicStreamPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
